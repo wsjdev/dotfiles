6731ed13496cd5075f5649271217247b337f130c
wsjdev <wsjdev@gmail.com>
2016-12-17 20:46:01 +0900

    linux では linux brew をインストールするように変更

---
+++ homebrew/path.zsh
@@ -0,0 +1,6 @@
+if [ "$(uname -s)" != "Darwin" ]
+then
+  export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
+  export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+fi
