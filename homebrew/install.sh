27139403f5dff2266090a193ba13a84c90725041
wsjdev <wsjdev@gmail.com>
2016-12-21 05:42:49 +0900

    linuxbrew のインストール処理を修正

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -16,9 +16,21 @@ then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
+    if test $(which apt-get)
+    then
+      command='apt-get'
+      sudo apt-get install -y build-essential
+    elif test $(which yum)
+    then
+      command='yum'
+      sudo yum groupinstall -y 'Development Tools'
+    fi
+
+    sudo $command install -y ruby
+    sudo $command install -y curl
+
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   fi
-
 fi
 
 exit 0
