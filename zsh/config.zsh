27d0ff95b99e7d5dc01e5a1d7adc3e16acab94de
wsjdev <wsjdev@gmail.com>
2017-01-04 04:23:06 +0900

    zsh 補完候補に色付け

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -45,3 +45,8 @@ bindkey '^?' backward-delete-char
 bindkey -v
 bindkey -v '^?' backward-delete-char
 bindkey -M viins '^g' vi-cmd-mode
+
+# ls の色
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+# ファイル補完候補に色を付ける
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
