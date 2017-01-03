921e375d6161479482c49ef468722f219b3113e9
wsjdev <wsjdev@gmail.com>
2017-01-04 04:33:08 +0900

    gls ではなく、通常の ls に色付け

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,14 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
+case ${OSTYPE} in
+  darwin*)
+    # Mac用の設定
+    export CLICOLOR=1
+    alias ls='ls -G -F'
+    ;;
+  linux*)
+    # Linux用の設定
+    alias ls='ls -F --color=auto'
+    ;;
+esac
+alias l="ls -lAh"
+alias ll="ls -l"
+alias la='ls -A'
