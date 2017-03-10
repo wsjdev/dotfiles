7ddc0d118140029269966427a638dd27a3e6412f
wsjdev <wsjdev@gmail.com>
2017-03-11 02:20:41 +0900

    c-w に backward-delete-word をバインド
    消せない時があったため

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -45,6 +45,7 @@ bindkey '^?' backward-delete-char
 bindkey -v
 bindkey -v '^?' backward-delete-char
 bindkey -v '^h' backward-delete-char
+bindkey -v '^w' backward-delete-word
 
 # ls の色
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
