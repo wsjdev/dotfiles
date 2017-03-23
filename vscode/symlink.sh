cdbb701e037b552b421d7c6fbfb827080f0d906e
wsjdev <wsjdev@gmail.com>
2017-03-24 00:31:56 +0900

    OS 判定の処理を != から == に変更
    世界には Linux と Darwin 以外にも OS が存在するため

--- vscode/symlink.sh
+++ vscode/symlink.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 file="settings.json"
-if [ "$(uname -s)" != "Darwin" ]
+if [ "$(uname -s)" == "Darwin" ]
 then
-  path="$HOME/.config/Code/User/"
-else
   path="$HOME/Library/Application Support/Code/User/"
+else
+  path="$HOME/.config/Code/User/"
 fi
 mkdir -p "$path"
 ln -sf "$ZSH/vscode/$file" "$path$file"
