add5a69e081e0a4f4e456ec13f87409ad8e251cd
wsjdev <wsjdev@gmail.com>
2017-04-02 00:03:32 +0900

    linux brew doctor に指摘された問題点を修正

--- homebrew/_path.zsh
+++ homebrew/_path.zsh
@@ -1,6 +1,8 @@
 if test "$(expr substr $(uname -s) 1 5)" = "Linux"
 then
   export PATH="$HOME/.linuxbrew/bin:$PATH"
+  export PATH="$HOME/.linuxbrew/sbin:$PATH"
   export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
   export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+  export XDG_DATA_DIRS="$HOME/.linuxbrew/share:$XDG_DATA_DIRS"
 fi
