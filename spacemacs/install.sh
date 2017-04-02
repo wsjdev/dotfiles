93bb60a349b560e8494a833b2d257a5f5200b181
wsjdev <wsjdev@gmail.com>
2017-04-02 11:30:40 +0900

    Linux の emacs は brew を使用せずにインストール

--- spacemacs/install.sh
+++ spacemacs/install.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-brew tap d12frosted/emacs-plus
-brew install emacs-plus
-
-if test "$(uname)" = "Darwin"
-then
-  brew linkapps emacs-plus
-fi
-
 if [ ! -e ~/.emacs.d ]
 then
   git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+fi
+
+if test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+    if test $(which apt-get)
+    then
+      COMMAND='apt-get'
+    elif test $(which yum)
+    then
+      COMMAND='yum'
+    fi
+  sudo ${COMMAND} install -y emacs
+elif test "$(uname)" = "Darwin"
+then
+  brew tap d12frosted/emacs-plus
+  brew install emacs-plus
+  brew linkapps emacs-plus
 fi
