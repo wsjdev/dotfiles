9fc5283ad82870796d0e63253aeb8b93082c9087
wsjdev <wsjdev@gmail.com>
2017-04-10 10:27:39 +0900

    Change zsh keybind from vim to emacs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -41,12 +41,6 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
 
-# vim 風キーバインドにする
-bindkey -v
-bindkey -v '^?' backward-delete-char
-bindkey -v '^h' backward-delete-char
-bindkey -v '^w' backward-delete-word
-
 # ls の色
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 # ファイル補完候補に色を付ける
